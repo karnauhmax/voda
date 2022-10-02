b5f78224905734ac3bf52f738ebdcbe1793d7936
Max Karnauh <krnhmx@gmail.com>
2022-10-02 12:08:58 +0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import Modal from "./functions/modals";
+import Tab from "./functions/tabs";
+import { burger } from "./functions/burger";
+import Splide from "@splidejs/splide";
+import DynamicAdapt from "./functions/dynamicAdapt";
