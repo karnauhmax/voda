25141676c119b196c8736783f2e74278b713c6c1
Max Karnauh <krnhmx@gmail.com>
2022-10-05 21:11:19 +0300

    minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,1 @@
-import Modal from "./functions/modals";
-import Tab from "./functions/tabs";
-import { burger } from "./functions/burger";
-import Splide from "@splidejs/splide";
-import DynamicAdapt from "./functions/dynamicAdapt";
+import "./_functions";
